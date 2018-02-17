04aacf338fa114cab59dcc7c891bc40213597753
Gabe <hgabka@dev.hgabka.eu>
2018-02-17 17:16:34 +0100

    emailek

---
+++ src/Model/BoolSettingType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hgabka\SettingsBundle\Model;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class BoolSettingType extends AbstractSettingType
+{
+    public function getId()
+    {
+        return 'bool';
+    }
+
+    public function getPriority()
+    {
+        return 3;
+    }
+
+    public function getFormType()
+    {
+        return CheckboxType::class;
+    }
+
+    public function transformValue($value)
+    {
+        return (bool)$value;
+    }
+
+    public function reverseTransformValue($value)
+    {
+        return (bool)$value;
+    }
+}
