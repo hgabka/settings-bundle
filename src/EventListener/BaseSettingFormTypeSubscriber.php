907edfcac0f9c593963e78cde2f50d46d6f5c94c
Gabe <hgabka@dev.hgabka.eu>
2018-02-18 19:29:53 +0100

    add constraint

--- src/EventListener/BaseSettingFormTypeSubscriber.php
+++ src/EventListener/BaseSettingFormTypeSubscriber.php
@@ -5,7 +5,11 @@ namespace Hgabka\SettingsBundle\EventListener;
 use Hgabka\SettingsBundle\Event\SettingFormTypeEvent;
 use Hgabka\SettingsBundle\Helper\SettingsManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Validator\Constraint;
 
+/**
+ * Class BaseSettingFormTypeSubscriber.
+ */
 abstract class BaseSettingFormTypeSubscriber implements EventSubscriberInterface
 {
     /** @var SettingsManager */
@@ -47,6 +51,36 @@ abstract class BaseSettingFormTypeSubscriber implements EventSubscriberInterface
         if (!empty($options) && is_array($options) && $builder) {
             foreach ($builder->all() as $name => $child) {
                 $this->replaceChild($event, $name, $options, $newType);
+            }
+        }
+    }
+
+    /**
+     * @param SettingFormTypeEvent $event
+     * @param string               $name
+     * @param array|Constraint     $constraints
+     */
+    protected function addConstraintsToChild(SettingFormTypeEvent $event, string $name, $constraints)
+    {
+        $builder = $event->getFormBuilder();
+        if (!empty($options) && is_array($options) && $builder && $builder->has($name)) {
+            $child = $builder->get('name');
+            $options = $child->getOptions();
+            $this->settingsManager->addConstraints($options, $constraints);
+            $builder->add($name, get_class($child->getType()->getInnerType()), $options);
+        }
+    }
+
+    /**
+     * @param SettingFormTypeEvent $event
+     * @param array|Constraint     $constraints
+     */
+    protected function addConstraints(SettingFormTypeEvent $event, $constraints)
+    {
+        $builder = $event->getFormBuilder();
+        if (!empty($options) && is_array($options) && $builder) {
+            foreach ($builder->all() as $name => $child) {
+                $this->addConstraintsToChild($event, $name, $constraints);
             }
         }
     }
