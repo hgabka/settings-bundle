d54cd0f67e49b526f2a7caa83ea7abade432ebaf
hgabka <hgabka@gmail.com>
2021-03-21 21:43:36 +0100

    Create WysiwygSettingType.php

---
+++ src/Model/WysiwygSettingType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hgabka\SettingsBundle\Model;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TextSettingType extends AbstractSettingType
+{
+    /**
+     * @return mixed|string
+     */
+    public function getId()
+    {
+        return 'text';
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getPriority()
+    {
+        return 1;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getFormType()
+    {
+        return TextareaType::class;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFormTypeOptions()
+    {
+        return ['attr' => [
+            'class' => 'form-control',
+        ]];
+    }
+}
