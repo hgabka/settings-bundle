eae8e5f2e5a9ad8340163677fff6d5430e1ef6f2
hgabka <hgabka@gmail.com>
2022-01-25 08:35:01 +0100

    Update HgabkaSettingsTwigExtension.php

--- src/Twig/HgabkaSettingsTwigExtension.php
+++ src/Twig/HgabkaSettingsTwigExtension.php
@@ -5,6 +5,7 @@ namespace Hgabka\SettingsBundle\Twig;
 use Hgabka\SettingsBundle\Entity\Setting;
 use Hgabka\SettingsBundle\Helper\SettingsManager;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class HgabkaSettingsTwigExtension extends AbstractExtension
@@ -25,11 +26,18 @@ class HgabkaSettingsTwigExtension extends AbstractExtension
     /**
      * @return array
      */
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('get_setting', [$this, 'getSetting']),
             new TwigFunction('is_setting_visible', [$this, 'isSettingVisible']),
+        ];
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('replace_settings', [$this, 'replaceSettings'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -48,6 +56,18 @@ class HgabkaSettingsTwigExtension extends AbstractExtension
         $type = $this->settingManager->getType($setting->getType());
 
         return $type && $type->isVisible();
+    }
+    
+    public function replaceSettings(string $target, string $prefix = '', string $postfix = ''): string
+    {
+        $pairs = [];
+        foreach ($this->settingManager->getValues() as $name => $value) {
+            if (is_scalar($value) && !is_bool($value)) {
+                $pairs[$prefix.$name.$postfix] = (string)$value;
+            }
+        }
+        
+        return strtr($target, $pairs);
     }
 
     /**
